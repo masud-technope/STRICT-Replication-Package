6defdedc2acf28607b596af8c9249adf72bfb50b
unknown <masudcseku@gmail.com>
2018-12-11 11:50:50 -0600

    practice results

---
+++ sample-output-Oct10-myputput.java
@@ -0,0 +1,118 @@
+===========193415===========
+TR: QE=1	Q:view disabled disconnect Account Bug Contacts view menu entries disabled logged
+POSR: QE=7	Q:accounts chevron Bug Contacts view menu entries disabled logged
+STRICT: QE=1	Q:Account chevron accounts view Bug Contacts view menu entries disabled logged
+-----------------------------
+TB: STRICT: QE=1	Q:Account chevron accounts view Bug Contacts view menu entries disabled logged
+BB: QE=5 Bug Contacts view menu entries disabled logged logged press chevron contacts view Add Account disconnect accounts shouldn disabled logged
+UB: QE=1  accounts
+
+
+===========194981===========
+TR: QE=3	Q:scrollback message Don view Bug Don automatically scroll message received
+POSR: QE=62	Q:message Bug Don automatically scroll message received
+STRICT: QE=3	Q:message scrollback Don view Bug Don automatically scroll message received
+--------------------------
+TB: STRICT: QE=3	Q:message scrollback Don view Bug Don automatically scroll message received
+BB: QE=22  Bug Don automatically scroll message received
+UB: QE=1 message  view  received
+
+
+===========195770===========
+TR: QE=1	Q:dialog support Browse browse Bug Browse Contacts dialog support filtering online active contacts
+POSR: QE=1	Q:support dialog Bug Browse Contacts dialog support filtering online active contacts
+STRICT: QE=16	Q:support dialog Contacts Browse Bug Browse Contacts dialog support filtering online active contacts
+-----------------------------
+TB: QE=1	Q:dialog support Browse browse Bug Browse Contacts dialog support filtering online active contacts
+BB: QE=9 	Bug Browse Contacts dialog support filtering online active contacts nifty browse dialog support ability filter contacts
+UB: QE=1  dialog Browse
+
+
+===========198012===========
+TR: QE=2	Q:connect localhost hyperlinks hyperlink Bug generic provider connect receive hyperlink
+POSR: QE=11	Q:Bug hyperlink Bug generic provider connect receive hyperlink
+STRICT: QE=12	Q:hyperlink hyperlinks server localhost Bug generic provider connect receive hyperlink
+----------------------------
+TB: QE=2	Q:connect localhost hyperlinks hyperlink Bug generic provider connect receive hyperlink
+BB: QE=14	Bug generic provider connect receive hyperlink ECF hyperlinks form ecftcp localhost server hyperlink activation Open connect dialog Set connect ecftcp localhost server current behavior
+UB: QE=1	connect receive hyperlink
+
+
+===========205532===========
+TR: QE=16	Q:subject URL chat channel Bug IRC add hyperlinking chat subject box
+POSR: QE=6	Q:chat URL channel account Bug IRC add hyperlinking chat subject box
+STRICT: QE=16	Q:URL chat subject channel hyperlinking Bug IRC add hyperlinking chat subject box
+--------------------------------
+TB: QE=6	Q:chat URL channel account Bug IRC add hyperlinking chat subject box
+BB: QE=14	Bug IRC add hyperlinking chat subject box nice subject topic main chat output window set subject account channel permissions cases subject URL handy URL hyperlinked URL easily accessed channel
+UB: QE=2, chat URL channel add chat
+
+
+===========211585===========
+TR: QE=3	Q:naming Discovery code authority Bug Discovery JMDNS parsing problem service type defines naming authority
+POSR: QE=4	Q:authority JMDNS Bug Bug Discovery JMDNS parsing problem service type defines naming authority
+STRICT: QE=4	Q:authority JMDNS Discovery Bug Bug Discovery JMDNS parsing problem service type defines naming authority
+----------------------------------
+TB: QE=3	Q:naming Discovery code authority Bug Discovery JMDNS parsing problem service type defines naming authority
+BB: QE=5	Bug Discovery JMDNS parsing problem service type defines naming authority parser code stumbles naming authority set
+UB: QE=2 naming Discovery JMDNS naming
+
+
+===========218142===========
+TR: QE=3	Q:dev Add priority documentation service Bug Add documentation javadoc service priority weight
+POSR: QE=8	Q:weight Bug documentation service Bug Add documentation javadoc service priority weight
+STRICT: QE=8	Q:documentation dev service weight priority Bug Add documentation javadoc service priority weight
+---------------------------------
+TB: QE=3	Q:dev Add priority documentation service Bug Add documentation javadoc service priority weight
+BB: QE=8	Bug Add documentation javadoc service priority weight
+UB: QE=1 	priority documentation javadoc service priority
+
+
+===========218584===========
+TR: QE=4	Q:remote methods interface parameters dev method type list choose invoke set values synchronous future listener asynchronous aid Bug remoteservices add invoking remote methods interface classes
+POSR: QE=3	Q:interface methods method OSGi parameters type values OSG mailing Bug remoteservices add invoking remote methods interface classes
+STRICT: QE=3	Q:interface methods method parameters type dev values OSG listener future OSGi eclipse org mhonarc remote aid list discovery Bug remoteservices add invoking remote methods interface classes
+---------------------------------
+TB: QE=3	Q:interface methods method OSGi parameters type values OSG mailing Bug remoteservices add invoking remote methods interface classes
+UB: QE=9	Bug remoteservices add invoking remote methods interface classes mailing list entry http dev eclipse org mhonarc lists ecf dev msg html helpful invoke methods interfaces expose local remote OSGi services basic requirements method names type signatures method parameters return values reflective access interface class Enter set values parameters Serializable type constrain type Choose method remote invocation proxy synchronous asynchronous listener future asynchresult Set choose timeout interface service discovered ECF discovery user developer decide methods invoke set parameters methods invoke remote method nice aid development testing debugging remote OSGi services
+UB: QE=2 interface methods OSGi parameters remoteservices add invoking
+
+
+===========222253===========
+TR: QE=15	Q:chat attachment irc Bug collab height irc collaboration chat input field
+POSR: QE=7	Q:chat attachment details Bug collab height irc collaboration chat input field
+STRICT: QE=2	Q:chat attachment collab Bug collab height irc collaboration chat input field
+--------------------------------
+TB: QE=2	Q:chat attachment collab Bug collab height irc collaboration chat input field
+BB: QE=6	Bug collab height irc collaboration chat input field Created attachment details irc chat png Build
+UB: QE=1 chat collab height irc input
+
+
+===========238970===========
+TR: QE=3	Q:CPU MSN bit report increases seconds component Bug MSN CPU usage increases connecting
+POSR: QE=5	Q:CPU MSN usage Eclipse bit Bug MSN CPU usage increases connecting
+STRICT: QE=8	Q:CPU MSN bit report usage Eclipse bug Bug MSN CPU usage increases connecting
+------------------------------------
+TB: QE=3	Q:CPU MSN bit report increases seconds component Bug MSN CPU usage increases connecting
+BB: QE=11	Bug MSN CPU usage increases connecting
+UB: QE=2 CPU MSN report component MSN CPU
+
+
+===========256980===========
+TR: QE=4	Q:presence contribute collab ecf Bug ecf collab contribute presence container
+POSR: QE=2	Q:collab presence items Bug ecf collab contribute presence container
+STRICT: QE=5	Q:presence collab items contributions Bug ecf collab contribute presence container
+---------------------------------
+TB: QE=2 collab presence items Bug ecf collab contribute presence container
+BB: QE=11 Bug ecf collab contribute presence container
+BM: QE=1 collab presence collab presence container
+
+
+===========292972===========
+TR: QE=9	Q:filetransfer patches number bug discuss Bug filetransfer apply additions fixes genuitec patch
+POSR: QE=9	Q:bug filetransfer additions Bug filetransfer apply additions fixes genuitec patch
+STRICT: QE=15	Q:filetransfer bug additions application patch Bug filetransfer apply additions fixes genuitec patch
+---------------------
+TB: QE=9	Q:bug filetransfer additions Bug filetransfer apply additions fixes genuitec patch
+BB: QE=22	Bug filetransfer apply additions fixes genuitec patch
+UB: QE=1 filetransfer  filetransfer fixes patch
